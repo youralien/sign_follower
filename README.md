8871d993e0e778901a9fc15c86f3801e31582764
youralien <ryan.louie@students.olin.edu>
2017-02-11 13:34:16 -0500

    Updated README + added street_sign_recognizer node
    Summary:
    README has overview, startup instructions, and HSV conversion section
    
    Starter code for node does not depend on Template Matcher. All flags
    work when set to True

--- README.md
+++ README.md
@@ -1,18 +1,90 @@
 # Sign Follower
 
-Here's a sample processing pipeline for doing robot sign following.
+By the end of the Traffic Sign Follower project, you will have a robot that will look like this! 
 
-* __Localize__ the sign.  How does one draw a bounding box around the sign?
-* __Recognize__ the sign's meaning. Is it telling me to turn right, left, or u-turn?  
-* __Navigate__ based on the sign's message. 
+**Video Demo:** [NEATO ROBOT OBEYS TRAFFIC SIGNS](https://youtu.be/poReVhj1lSA)
 
-## Localization
+## System
+![alt text][system-overview]
 
-The Goal:
+[system-overview]: images/vision-nav-system-overview.png "Three stages of the vision and navigation system: 1) waypoint navigation 2) sign recognition, and 3) sign obeyance via changing the next waypoint"
+
+Navigation and mapping is handled by the built-in ROS package ```neato_2dnav``` .  Mapping the location of the robot in the environment was handled by [```gmapping```](http://wiki.ros.org/gmapping), a package that provides laser-based SLAM (simultaneous localization and mapping).  Navigation was handled by the [```move_base```](http://wiki.ros.org/move_base) package;   our program published waypoints to the ```/move_base_simple/goal``` topic while the internals of path planning and obstacle avoidance were abstracted away.
+
+You will put your comprobo-chops to the test by developing a sign detection node which publishes to a topic, ```/predicted_sign```, once it is confident about recognizing the traffic sign in front of it. 
+
+### Running and Developing the street_sign_recognizer node
+
+We've provided some rosbags that will get you going.
+
+[uturn.bag](https://drive.google.com/open?id=0B85lERk460TUYjFGLVg1RXRWams)
+[rightturn.bag](https://drive.google.com/open?id=0B85lERk460TUN3ZmUk15dmtPTFk)
+[leftturn.bag](https://drive.google.com/open?id=0B85lERk460TUTkdTQW5yQ0FwSEE)
+
+Start by replaying these rosbags on loop:
+
+```
+rosbag play uturn.bag -l
+```
+
+They have `/camera/image_raw/compressed` channel recorded. In order to republish the compressed image as a raw image, 
+
+```
+rosrun image_transport republish compressed in:=/camera/image_raw _image_transport:=compressed raw out:=/camera/image_raw
+```
+
+To run the node,
+
+```
+rosrun sign_follower street_sign_recognizer.py
+```
+
+If you ran on the steps above correctly, a video window should appear visualizing the Neato moving towards a traffic sign. 
+
+### Detecting Signs in Scene Images
+Reliable detection of traffic signs and creating accurate bounding box crops is an important preprocessing step for further steps in the data pipeline.
+
+You will implement code that will find the bounding box around the traffic sign in a scene. We've outlined a suggested data processing pipeline for this task.
+
+1. colorspace conversion to hue, saturation, value (```hsv_image``` seen in the top left window). 
+2. a filter is applied that selects only for objects in the yellow color spectrum. The range of this spectrum can be found using hand tuning (```binarized_image``` seen in the bottom window). 
+3. a bounding box is drawn around the most dense, yellow regions of the image.
 
 ![][yellow_sign_detector]
+[yellow_sign_detector]: images/yellow-sign-detector.gif "Bounding box generated around the yellow parts of the image.  The video is converted to HSV colorspace, an inRange operation is performed to filter out any non yellow objects, and finally a bounding box is generated."
 
-#### Some guidance
+__DELIVERABLE FOR THIS SECTION__: Visualize all these steps and save those results as a screenshot, screen capture, or saved image. And of course, write the code that produces these results.
+
+#### Red-Green-Blue to Hue-Saturation-Value Images
+
+There are different ways to represent the information in an image. A gray-scale iamge has `(n_rows, n_cols)`. An rgb image has shape `(n_rows, n_cols, 3)` since it has three channels: red, green, and blue.
+
+Color images are also represented in different ways too.  Aside from the default RGB colorspace, there exists alot of others. We'll be focused on using [HSV/HSL](https://en.wikipedia.org/wiki/HSL_and_HSV): Hue, Saturation, and Value/Luminosity. Like RGB, a HSV image has three channels and is shape `(n_rows, n_cols, 3)`. The hue channel is well suited for color detection tasks, because we can filter by color on a single dimension of measurement, and it is a measure that is invariant to lighting conditions.  
+
+[OpenCV provides methods to convert images from one color space to another.](http://docs.opencv.org/2.4/modules/imgproc/doc/miscellaneous_transformations.html#cvtcolor).
+
+You will be writing all your image processing pipeline within the `process_image` callback function. Here is what the starter code looks like so far.
+
+```python
+    def process_image(self, msg):
+        """ Process image messages from ROS and stash them in an attribute
+            called cv_image for subsequent processing """
+        self.cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
+
+        (...)
+
+        # Creates a window and displays the image
+        cv2.imshow('video_window', self.cv_image)
+        cv2.waitKey(5)
+```
+
+A good first step would be convert `self.cv_image` into an HSV image and visualize it. Like any good roboticist, visualize everything to make sure it meets your expectations.
+
+#### Filtering the image for only yellow
+
+Since the set of signs we are recognizing are all yellow, by design, we can handtune a filter that will only select the certain shade of yellow in our image.
+
+The starter code provides some helper methods
 
 * What are some distinguishing visual features about the sign?  Is there similarities in color and/or geometry?
 * Since we interested in generating a bounding box to be used in cropping out the sign from the original frame, what are different methods of generating candidate boxes?
@@ -21,6 +93,7 @@ The Goal:
 One method that could be fruitful would be dividing the image in a grid.  The object then is to decide which grid cells contain the region of interest.
 
 ![][grid]
+[grid]: images/grid.png
 
 Some suggested deliverables for this step
 1. Use the provided test images `images/leftturn_scene.jpg` to develop a sign localization algorithm.
@@ -29,5 +102,3 @@ Some suggested deliverables for this step
 ## Recognition
 
 ## Navigating
-[yellow_sign_detector]: images/yellow-sign-detector.gif
-[grid]: images/grid.png
---
+++ scripts/street_sign_recognizer.py
@@ -0,0 +1,196 @@
+#!/usr/bin/env python
+
+""" This is a script that walks through some of the basics of working with images
+    with opencv in ROS. """
+
+import rospy
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+from std_msgs.msg import String, Bool
+import cv2
+import numpy as np
+import os
+
+# TODO: uncomment this line once you have TemplateMatcher working
+# from template_matcher import TemplateMatcher
+
+
+class StreetSignRecognizer(object):
+    """ This robot should recognize street signs """
+
+
+    curr_dir = os.path.dirname(os.path.realpath(__file__))
+    template_images = {
+        "lturn":  os.path.join(curr_dir, '../images/leftturn_box_small.png'),
+        "rturn": os.path.join(curr_dir, '../images/rightturn_box_small.png'),
+        "uturn": os.path.join(curr_dir, '../images/uturn_box_small.png')
+    }
+
+    def __init__(self, image_topic, sleep_topic):
+        """ Initialize the street sign reocgnizer """
+        rospy.init_node('street_sign_recognizer')
+        self.cv_image = None                        # the latest image from the camera
+        self.bridge = CvBridge()                    # used to convert ROS messages to OpenCV
+        self.saveCounter = 0                        # how many images we've saved to disk
+
+
+        # TODO: uncomment these lines once you have TemplateMatcher working
+        # print "Loading TemplateMatcher"
+        # self.template_matcher = TemplateMatcher(self.template_images)
+
+        self.pub = rospy.Publisher('predicted_sign', String, queue_size=1)
+        cv2.namedWindow('video_window')
+
+        self.sleeping = False
+
+        self.running_predictions = {"lturn": 0, "rturn": 0, "uturn": 0}
+
+        # 'use' parameters for quick changes in node functionality
+        self.use_slider = False
+        self.use_mouse_hover = False
+        self.use_saver = False
+        self.use_predict = False
+
+        # threshold by which the running confidence summation must achieve to publish a predicted_sign
+        # TODO: hand tune this value with testing
+        self.decision_threshold = 35
+
+        if self.use_mouse_hover:
+            # when mouse hovers over video window
+            cv2.setMouseCallback('video_window', self.process_mouse_event)
+
+        if self.use_slider:
+            cv2.namedWindow('threshold_image')
+            self.hsv_lb = np.array([0, 0, 0])
+            cv2.createTrackbar('H lb', 'threshold_image', 0, 255, self.set_h_lb)
+            cv2.createTrackbar('S lb', 'threshold_image', 0, 255, self.set_s_lb)
+            cv2.createTrackbar('V lb', 'threshold_image', 0, 255, self.set_v_lb)
+            self.hsv_ub = np.array([255, 255, 255])
+            cv2.createTrackbar('H ub', 'threshold_image', 0, 255, self.set_h_ub)
+            cv2.createTrackbar('S ub', 'threshold_image', 0, 255, self.set_s_ub)
+            cv2.createTrackbar('V ub', 'threshold_image', 0, 255, self.set_v_ub)
+
+        rospy.Subscriber(image_topic, Image, self.process_image)
+        rospy.Subscriber(sleep_topic, Bool, self.process_sleep)
+
+    # # # # # # # # # #
+    # color callbacks #
+    # # # # # # # # # #
+
+    def set_h_lb(self, val):
+        """ set hue lower bound """
+        self.hsv_lb[0] = val
+
+    def set_s_lb(self, val):
+        """ set saturation lower bound """
+        self.hsv_lb[1] = val
+
+    def set_v_lb(self, val):
+        """ set value lower bound """
+        self.hsv_lb[2] = val
+
+    def set_h_ub(self, val):
+        """ set hue upper bound """
+        self.hsv_ub[0] = val
+
+    def set_s_ub(self, val):
+        """ set saturation upper bound """
+        self.hsv_ub[1] = val
+
+    def set_v_ub(self, val):
+        """ set value upper bound """
+        self.hsv_ub[2] = val
+
+    def process_sleep(self, msg):
+        """ Process sleep messages from the navigation node and stash them
+            in an attribute called sleeping """
+        self.sleeping = msg.data
+
+    def process_image(self, msg):
+        """ Process image messages from ROS and stash them in an attribute
+            called cv_image for subsequent processing """
+        self.cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
+
+        pt1, pt2 = self.sign_bounding_box()
+
+        # crop and gray scale the bounding box region of interest
+        cropped_sign = self.cv_image[pt1[1]:pt2[1], pt1[0]:pt2[0]]
+        cropped_sign_gray = cv2.cvtColor(cropped_sign, cv2.COLOR_BGR2GRAY)
+
+        # draw bounding box rectangle
+        cv2.rectangle(self.cv_image, pt1, pt2, color=(0, 0, 255), thickness=5)
+        cv2.imshow('video_window', self.cv_image)
+
+        # displays the image for a X milliseconds 
+        cv2.waitKey(5)
+
+        # saving frames of video as images can be a useful format to work with
+        # i.e. if you want to load the image frame in a jupyter notebook
+        if self.use_saver:
+            cv2.imwrite("/tmp/bin_img_{0:0>4}.jpg".format(self.saveCounter), cropped_sign)
+            self.saveCounter += 1
+
+        if self.use_predict and pt1 and pt2:
+            # make predictions with confidence for each sign key
+            # TODO: uncomment the predict call once template matcher is working
+            # prediction = self.template_matcher.predict(cropped_sign_gray)
+            
+            # TODO: remove this dummy prediction
+            prediction = {'rturn': 0.0, 'lturn': 0.0, 'rturn': 0.0}
+
+            for sign_key in prediction:
+                self.running_predictions[sign_key] += prediction[sign_key]
+
+    def process_mouse_event(self, event, x,y,flags,param):
+        """ Process mouse events so that you can see the color values associated
+            with a particular pixel in the camera images """
+        image_info_window = 255*np.ones((500,500,3))
+
+        # show bgr values
+        cv2.putText(image_info_window,
+                    'Color (b=%d,g=%d,r=%d)' % (self.cv_image[y,x,0], self.cv_image[y,x,1], self.cv_image[y,x,2]),
+                    (5,100),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    1,
+                    (0,0,0))
+
+        # TODO: show hsv values
+
+        cv2.imshow('image_info', image_info_window)
+        cv2.waitKey(5)
+
+    def run(self):
+        """ The main run loop"""
+        r = rospy.Rate(5)
+        while not rospy.is_shutdown():
+            if not self.sleeping:
+                # TODO: publish to the /predicted_sign topic once confident
+                # running_predictions is dictionary, with
+                # keys being 'lturn', 'rturn', and 'uturn',
+                # values being cumulative prediction confidences.
+                # Use some logic like...
+                # if running_predictions[sign_key] > self.decision_threshold:
+                #   publish(sign_key)
+                #   reset running_predictions to 0 again
+                pass
+            else:
+                # Don't store running predictions while sleeping
+                self.running_predictions = {"lturn": 0, "rturn": 0, "uturn": 0}
+
+            r.sleep()
+
+    def sign_bounding_box(self):
+        """
+        Returns
+        -------
+        (pt1, pt2) where pt1 and pt2 are tuples of (x_pixel, y_pixel)
+            defining topleft and bottomright corners of the bounding box 
+        """
+        # TODO: YOUR SOLUTION HERE
+        pt1 = (200, 200)
+        pt2 = (400, 400)
+        return pt1, pt2
+    
+if __name__ == '__main__':
+    node = StreetSignRecognizer("/camera/image_raw","/imSleeping")
+    node.run()
