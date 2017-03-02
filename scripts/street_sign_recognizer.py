e46abc8d840ae2c6ebfcd791517c5b107d215bf5
Jordan Van Duyne <jordannn808@gmail.com>
2017-03-02 14:25:03 -0500

    work on making binarized image

--- scripts/street_sign_recognizer.py
+++ scripts/street_sign_recognizer.py
@@ -21,13 +21,29 @@ class StreetSignRecognizer(object):
         self.bridge = CvBridge()                    # used to convert ROS messages to OpenCV
         cv2.namedWindow('video_window')
         cv2.namedWindow('hsv_window')
+        cv2.namedWindow('threshold_image')
+        self.hsv_lb = np.array([0, 0, 0]) # hsv lower bound
+        cv2.createTrackbar('H lb', 'threshold_image', 0, 255, self.set_h_lb)
+        cv2.createTrackbar('S lb', 'threshold_image', 0, 255, self.set_s_lb)
+        cv2.createTrackbar('V lb', 'threshold_image', 0, 255, self.set_v_lb)
+        self.hsv_ub = np.array([255, 255, 255]) # hsv upper bound
+        cv2.createTrackbar('H ub', 'threshold_image', 0, 255, self.set_h_ub)
+        cv2.createTrackbar('S ub', 'threshold_image', 0, 255, self.set_s_ub)
+        cv2.createTrackbar('V ub', 'threshold_image', 0, 255, self.set_v_ub)
         rospy.Subscriber("/camera/image_raw", Image, self.process_image)
+        self.image_info_window = None
+        cv2.setMouseCallback('video_window', self.process_mouse_event)
+        cv2.setMouseCallback('hsv_window', self.process_mouse_event)
+
 
     def process_image(self, msg):
         """ Process image messages from ROS and stash them in an attribute
             called cv_image for subsequent processing """
         self.cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
         self.hsv_image = cv2.cvtColor(self.cv_image, cv2.COLOR_BGR2HSV)
+        self.binary_image = cv2.inRange(self.hsv_image,
+                                        (self.hsv_lb[0], self.hsv_lb[1]], self.hsv_lb[2]),
+                                        (self.hsv_ub[0], self.hsv_ub[1], self.hsv_ub[2]))
 
         left_top, right_bottom = self.sign_bounding_box()
         left, top = left_top
@@ -51,6 +67,52 @@ class StreetSignRecognizer(object):
         right_bottom = (400, 400)
         return left_top, right_bottom
 
+    def process_mouse_events(self, event, x, y, flags, param):
+        """ Process mouse events so that you can see the color values associated
+            with a particular pixel in the camera images """
+        self.image_info_window = 255 * np.ones((500, 500, 3))
+
+        # show hsv values
+        cv2.putText(self.image_info_window,
+                'Color (h=%d,s=%d,v=%d)' % (self.hsv_image[y,x,0], self.hsv_image[y,x,1], self.hsv_image[y,x,2]),
+                (5,50), # 5 = x, 50 = y
+                cv2.FONT_HERSHEY_SIMPLEX,
+                1,
+                (0,0,0))
+
+        # show bgr values
+        cv2.putText(self.image_info_window,
+                'Color (b=%d,g=%d,r=%d)' % (self.cv_image[y,x,0], self.cv_image[y,x,1], self.cv_image[y,x,2]),
+                (5,100), # 5 = x, 50 = y
+                cv2.FONT_HERSHEY_SIMPLEX,
+                1,
+                (0,0,0))
+
+    def set_h_lb(self, val):
+        """ set hue lower bound """
+        self.hsv_lb[0] = val
+
+    def set_s_lb(self, val):
+        """ set saturation lower bound """
+        self.hsv_lb[1] = val
+
+    def set_v_lb(self, val):
+        """ set value lower bound """
+        self.hsv_lb[2] = val
+
+    def set_h_ub(self, val):
+        """ set hue upper bound """
+        self.hsv_ub[0] = val
+
+    def set_s_ub(self, val):
+        """ set saturation upper bound """
+        self.hsv_ub[1] = val
+
+    def set_v_ub(self, val):
+        """ set value upper bound """
+        self.hsv_ub[2] = val
+
+
     def run(self):
         """ The main run loop"""
         r = rospy.Rate(10)
@@ -60,6 +122,9 @@ class StreetSignRecognizer(object):
                 # creates a window and displays the image for X milliseconds
                 cv2.imshow('video_window', self.cv_image)
                 cv2.imshow('hsv_window', self.hsv_image)
+                cv2.waitKey(5)
+            if not self.image_info_window is None:
+                cv2.imshow('image_info', self.image_info_window)
                 cv2.waitKey(5)
             r.sleep()
 
