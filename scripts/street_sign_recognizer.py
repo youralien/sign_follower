9edaca64991bc5afbabb833a5526b0c985f5f379
Kim Asenbeck <kasenbec@wellesley.edu>
2017-03-03 07:34:46 -0500

    detection is working!

--- scripts/street_sign_recognizer.py
+++ scripts/street_sign_recognizer.py
@@ -8,6 +8,7 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import cv2
 import numpy as np
+from copy import deepcopy
 
 class StreetSignRecognizer(object):
     """ This robot should recognize street signs """
@@ -20,12 +21,49 @@ class StreetSignRecognizer(object):
         self.bridge = CvBridge()                    # used to convert ROS messages to OpenCV
         cv2.namedWindow('video_window')
         rospy.Subscriber("/camera/image_raw", Image, self.process_image)
+        cv2.namedWindow('threshold_image')
+        self.hsv_lb = np.array([0, 0, 0]) # hsv lower bound
+        cv2.createTrackbar('H lb', 'threshold_image', 0, 255, self.set_h_lb)
+        cv2.createTrackbar('S lb', 'threshold_image', 0, 255, self.set_s_lb)
+        cv2.createTrackbar('V lb', 'threshold_image', 0, 255, self.set_v_lb)
+        self.hsv_ub = np.array([255, 255, 255]) # hsv upper bound
+        cv2.createTrackbar('H ub', 'threshold_image', 0, 255, self.set_h_ub)
+        cv2.createTrackbar('S ub', 'threshold_image', 0, 255, self.set_s_ub)
+        cv2.createTrackbar('V ub', 'threshold_image', 0, 255, self.set_v_ub)        
 
+    def set_h_lb(self, val):
+        """ set hue lower bound """
+        self.hsv_lb[0] = val
+
+    def set_s_lb(self, val):
+        """ set saturation lower bound """
+        self.hsv_lb[1] = val
+
+    def set_v_lb(self, val):
+        """ set value lower bound """
+        self.hsv_lb[2] = val
+
+    def set_h_ub(self, val):
+        """ set hue upper bound """
+        self.hsv_ub[0] = val
+
+    def set_s_ub(self, val):
+        """ set saturation upper bound """
+        self.hsv_ub[1] = val
+
+    def set_v_ub(self, val):
+        """ set value upper bound """
+        self.hsv_ub[2] = val
+        
     def process_image(self, msg):
         """ Process image messages from ROS and stash them in an attribute
             called cv_image for subsequent processing """
         self.cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
-
+        self.hsv_image = cv2.cvtColor(self.cv_image, cv2.COLOR_BGR2HSV)
+        #self.binary_image = cv2.inRange(self.hsv_image, self.hsv_lb, self.hsv_ub)
+        #note: good ranges = h lb = 0; s lb = 75; v lb = 195; h ub = 32; s ub = 255; v ub = 244
+        self.binary_image = cv2.inRange(self.hsv_image, (0,75,220), (32,255,244))
+        
         left_top, right_bottom = self.sign_bounding_box()
         left, top = left_top
         right, bottom = right_bottom
@@ -43,9 +81,13 @@ class StreetSignRecognizer(object):
         (left_top, right_bottom) where left_top and right_bottom are tuples of (x_pixel, y_pixel)
             defining topleft and bottomright corners of the bounding box
         """
-        # TODO: YOUR SOLUTION HERE
-        left_top = (200, 200)
-        right_bottom = (400, 400)
+        image = deepcopy(self.binary_image)
+        contours = cv2.findContours(image, 1, 2)
+        cnt = contours[0]
+        x,y,w,h = cv2.boundingRect(cnt)
+        
+        left_top = (x, y)
+        right_bottom = (x+w, y+h)
         return left_top, right_bottom
 
     def run(self):
@@ -53,9 +95,10 @@ class StreetSignRecognizer(object):
         r = rospy.Rate(10)
         while not rospy.is_shutdown():
             if not self.cv_image is None:
-                print "here"
                 # creates a window and displays the image for X milliseconds
                 cv2.imshow('video_window', self.cv_image)
+                cv2.imshow('hsv_window', self.hsv_image)
+                cv2.imshow('dynamic', self.binary_image)
                 cv2.waitKey(5)
             r.sleep()
 
