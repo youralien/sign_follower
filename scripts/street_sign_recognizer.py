608d54feec2db71d88b863bdf5b5e324fe714267
nathanyee <nathaniel.yee@students.olin.edu>
2017-03-04 16:21:39 -0500

    working sliders

--- scripts/street_sign_recognizer.py
+++ scripts/street_sign_recognizer.py
@@ -24,6 +24,40 @@ class StreetSignRecognizer(object):
         self.image_info_window = None
         cv2.setMouseCallback('video_window', self.process_mouse_event)
 
+        cv2.namedWindow('threshold_image')
+        self.hsv_lb = np.array([0, 0, 0]) # hsv lower bound
+        cv2.createTrackbar('H lb', 'threshold_image', 0, 255, self.set_h_lb)
+        cv2.createTrackbar('S lb', 'threshold_image', 0, 255, self.set_s_lb)
+        cv2.createTrackbar('V lb', 'threshold_image', 0, 255, self.set_v_lb)
+        self.hsv_ub = np.array([255, 255, 255]) # hsv upper bound
+        cv2.createTrackbar('H ub', 'threshold_image', 0, 255, self.set_h_ub)
+        cv2.createTrackbar('S ub', 'threshold_image', 0, 255, self.set_s_ub)
+        cv2.createTrackbar('V ub', 'threshold_image', 0, 255, self.set_v_ub)
+
+    def set_h_lb(self, val):
+        """ set hue lower bound """
+        self.hsv_lb[0] = val
+
+    def set_s_lb(self, val):
+        """ set saturation lower bound """
+        self.hsv_lb[1] = val
+
+    def set_v_lb(self, val):
+        """ set value lower bound """
+        self.hsv_lb[2] = val
+
+    def set_h_ub(self, val):
+        """ set hue upper bound """
+        self.hsv_ub[0] = val
+
+    def set_s_ub(self, val):
+        """ set saturation upper bound """
+        self.hsv_ub[1] = val
+
+    def set_v_ub(self, val):
+        """ set value upper bound """
+        self.hsv_ub[2] = val
+
     def process_image(self, msg):
         """ Process image messages from ROS and stash them in an attribute
             called cv_image for subsequent processing """
