2c8fa8e46a0734cbdd4a47eefbb779c93770cc10
nathanyee <nathaniel.yee@students.olin.edu>
2017-03-04 16:42:36 -0500

    moved color slider to new object

--- scripts/street_sign_recognizer.py
+++ scripts/street_sign_recognizer.py
@@ -8,13 +8,18 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import cv2
 import numpy as np
+from color_slider import Color_Slider
 
-class StreetSignRecognizer(object):
+class StreetSignRecognizer(Color_Slider):
     """ This robot should recognize street signs """
 
 
     def __init__(self):
         """ Initialize the street sign reocgnizer """
+
+        #initialize the color slider
+        super(StreetSignRecognizer, self).__init__()
+
         rospy.init_node('street_sign_recognizer')
         self.cv_image = None                        # the latest image from the camera
         self.bridge = CvBridge()                    # used to convert ROS messages to OpenCV
@@ -24,46 +29,16 @@ class StreetSignRecognizer(object):
         self.image_info_window = None
         cv2.setMouseCallback('video_window', self.process_mouse_event)
 
-        cv2.namedWindow('threshold_image')
-        self.hsv_lb = np.array([0, 0, 0]) # hsv lower bound
-        cv2.createTrackbar('H lb', 'threshold_image', 0, 255, self.set_h_lb)
-        cv2.createTrackbar('S lb', 'threshold_image', 0, 255, self.set_s_lb)
-        cv2.createTrackbar('V lb', 'threshold_image', 0, 255, self.set_v_lb)
-        self.hsv_ub = np.array([255, 255, 255]) # hsv upper bound
-        cv2.createTrackbar('H ub', 'threshold_image', 0, 255, self.set_h_ub)
-        cv2.createTrackbar('S ub', 'threshold_image', 0, 255, self.set_s_ub)
-        cv2.createTrackbar('V ub', 'threshold_image', 0, 255, self.set_v_ub)
-
-    def set_h_lb(self, val):
-        """ set hue lower bound """
-        self.hsv_lb[0] = val
-
-    def set_s_lb(self, val):
-        """ set saturation lower bound """
-        self.hsv_lb[1] = val
-
-    def set_v_lb(self, val):
-        """ set value lower bound """
-        self.hsv_lb[2] = val
-
-    def set_h_ub(self, val):
-        """ set hue upper bound """
-        self.hsv_ub[0] = val
-
-    def set_s_ub(self, val):
-        """ set saturation upper bound """
-        self.hsv_ub[1] = val
-
-    def set_v_ub(self, val):
-        """ set value upper bound """
-        self.hsv_ub[2] = val
 
     def process_image(self, msg):
         """ Process image messages from ROS and stash them in an attribute
             called cv_image for subsequent processing """
         self.cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
         self.hsv_image = cv2.cvtColor(self.cv_image, cv2.COLOR_BGR2HSV)
-        self.binary_image = cv2.inRange(self.hsv_image, (20,128,128), (35,255,255))
+        # self.binary_image = cv2.inRange(self.hsv_image, (20,195,135), (34,255,238))
+
+        self.binary_image = cv2.inRange(self.hsv_image, (self.hsv_lb[0],self.hsv_lb[1],self.hsv_lb[2]), (self.hsv_ub[0],self.hsv_ub[1],self.hsv_ub[2]))
+
 
         left_top, right_bottom = self.sign_bounding_box()
         left, top = left_top
