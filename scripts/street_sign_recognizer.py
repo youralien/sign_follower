8c03dc27833461c1309034e2ffc92bdf207585db
nathanyee <nathaniel.yee@students.olin.edu>
2017-03-04 16:09:56 -0500

    displaying binary image filtered hsv

--- scripts/street_sign_recognizer.py
+++ scripts/street_sign_recognizer.py
@@ -21,10 +21,15 @@ class StreetSignRecognizer(object):
         cv2.namedWindow('video_window')
         rospy.Subscriber("/camera/image_raw", Image, self.process_image)
 
+        self.image_info_window = None
+        cv2.setMouseCallback('video_window', self.process_mouse_event)
+
     def process_image(self, msg):
         """ Process image messages from ROS and stash them in an attribute
             called cv_image for subsequent processing """
         self.cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
+        self.hsv_image = cv2.cvtColor(self.cv_image, cv2.COLOR_BGR2HSV)
+        self.binary_image = cv2.inRange(self.hsv_image, (20,128,128), (35,255,255))
 
         left_top, right_bottom = self.sign_bounding_box()
         left, top = left_top
@@ -48,6 +53,27 @@ class StreetSignRecognizer(object):
         right_bottom = (400, 400)
         return left_top, right_bottom
 
+    def process_mouse_event(self, event, x,y,flags,param):
+        """ Process mouse events so that you can see the color values associated
+        with a particular pixel in the camera images """
+        self.image_info_window = 255*np.ones((500,500,3))
+
+        # show hsv values
+        cv2.putText(self.image_info_window,
+        'Color (h=%d,s=%d,v=%d)' % (self.hsv_image[y,x,0], self.hsv_image[y,x,1], self.hsv_image[y,x,2]),
+        (5,50), # 5 = x, 50 = y
+        cv2.FONT_HERSHEY_SIMPLEX,
+        1,
+        (0,0,0))
+
+        # show bgr values
+        cv2.putText(self.image_info_window,
+        'Color (b=%d,g=%d,r=%d)' % (self.cv_image[y,x,0], self.cv_image[y,x,1], self.cv_image[y,x,2]),
+        (5,100),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        1,
+        (0,0,0))
+
     def run(self):
         """ The main run loop"""
         r = rospy.Rate(10)
@@ -55,7 +81,10 @@ class StreetSignRecognizer(object):
             if not self.cv_image is None:
                 print "here"
                 # creates a window and displays the image for X milliseconds
-                cv2.imshow('video_window', self.cv_image)
+                cv2.imshow('video_window', self.binary_image)
+                cv2.waitKey(5)
+            if not self.image_info_window is None:
+                cv2.imshow('image_info', self.image_info_window)
                 cv2.waitKey(5)
             r.sleep()
 
