c6cf683372e2d18b1b92e0d15d4b33fea893d08f
buttegab <gabriel.butterick@students.olin.edu>
2017-03-05 17:41:55 -0500

    finished assignment for the most part

--- scripts/street_sign_recognizer.py
+++ scripts/street_sign_recognizer.py
@@ -17,21 +17,57 @@ class StreetSignRecognizer(object):
         """ Initialize the street sign reocgnizer """
         rospy.init_node('street_sign_recognizer')
         self.cv_image = None                        # the latest image from the camera
+        self.hsv_image = None
         self.bridge = CvBridge()                    # used to convert ROS messages to OpenCV
         cv2.namedWindow('video_window')
         rospy.Subscriber("/camera/image_raw", Image, self.process_image)
+        
+        #init code for finding hsv under mouse in image
+        # self.image_info_window = None
+        # cv2.setMouseCallback('video_window', self.process_mouse_event)
+
+
+        #init code for sliders
+        # self.set_h_lb = 0
+        # self.set_s_lb = 0
+        # self.set_v_lb = 0
+        # self.set_h_ub = 0
+        # self.set_s_ub = 0
+        # self.set_v_ub = 0
+        cv2.namedWindow('threshold_image')
+        self.hsv_lb = np.array([0, 0, 0]) # hsv lower bound
+        cv2.createTrackbar('H lb', 'threshold_image', 0, 255, self.set_h_lb)
+        cv2.createTrackbar('S lb', 'threshold_image', 0, 255, self.set_s_lb)
+        cv2.createTrackbar('V lb', 'threshold_image', 0, 255, self.set_v_lb)
+        self.hsv_ub = np.array([255, 255, 255]) # hsv upper bound
+        cv2.createTrackbar('H ub', 'threshold_image', 0, 255, self.set_h_ub)
+        cv2.createTrackbar('S ub', 'threshold_image', 0, 255, self.set_s_ub)
+        cv2.createTrackbar('V ub', 'threshold_image', 0, 255, self.set_v_ub)
+
 
     def process_image(self, msg):
         """ Process image messages from ROS and stash them in an attribute
             called cv_image for subsequent processing """
         self.cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
 
+        self.hsv_image = cv2.cvtColor(self.cv_image, cv2.COLOR_BGR2HSV)
+        # self.binary_image = cv2.inRange(self.hsv_image, 
+        #     (self.hsv_lb[0],self.hsv_lb[1],self.hsv_lb[2]), 
+        #     (self.hsv_ub[0],self.hsv_ub[1],self.hsv_ub[2]))
+        self.binary_image = cv2.inRange(self.hsv_image, 
+            (25,176,77), 
+            (158,255,236))
+
+        #proposed h range: 25-30; proposed saturation range: 230-270 ;proposed value range: 210-235
+
+
+
         left_top, right_bottom = self.sign_bounding_box()
         left, top = left_top
         right, bottom = right_bottom
 
         # crop bounding box region of interest
-        cropped_sign = self.cv_image[top:bottom, left:right]
+        self.cropped_sign = self.cv_image[top:bottom, left:right]
 
         # draw bounding box rectangle
         cv2.rectangle(self.cv_image, left_top, right_bottom, color=(0, 0, 255), thickness=5)
@@ -43,19 +79,86 @@ class StreetSignRecognizer(object):
         (left_top, right_bottom) where left_top and right_bottom are tuples of (x_pixel, y_pixel)
             defining topleft and bottomright corners of the bounding box
         """
-        # TODO: YOUR SOLUTION HERE
-        left_top = (200, 200)
-        right_bottom = (400, 400)
+        #calculate contour and generate bounding rectangle
+        ret, thresh = cv2.threshold(self.binary_image,127,255,cv2.THRESH_BINARY)
+        contours, hierarchy = cv2.findContours(thresh, 1, 2)
+        cnts = sorted(contours, key = cv2.contourArea, reverse = True)[:10]
+        cnt = cnts[0]
+        
+
+
+        x,y,w,h = cv2.boundingRect(cnt)
+        print x,y,(x+w),(y+h)
+        # cv2.rectangle(self.binary_image,(x,y),(x+w,y+h),(0,255,0),2)
+        left_top = (x, y)
+        right_bottom = (x+w, y+h)
         return left_top, right_bottom
+
+    # def process_mouse_event(self, event, x,y,flags,param):
+    #     """ Process mouse events so that you can see the color values associated
+    #         with a particular pixel in the camera images """
+    #     self.image_info_window = 255*np.ones((500,500,3))
+
+    #     # show hsv values
+    #     cv2.putText(self.image_info_window,
+    #                 'Color (h=%d,s=%d,v=%d)' % (self.hsv_image[y,x,0], self.hsv_image[y,x,1], self.hsv_image[y,x,2]),
+    #                 (5,50), # 5 = x, 50 = y
+    #                 cv2.FONT_HERSHEY_SIMPLEX,
+    #                 1,
+    #                 (0,0,0))
+
+    #     # show bgr values
+    #     cv2.putText(self.image_info_window,
+    #                 'Color (b=%d,g=%d,r=%d)' % (self.cv_image[y,x,0], self.cv_image[y,x,1], self.cv_image[y,x,2]),
+    #                 (5,100),
+    #                 cv2.FONT_HERSHEY_SIMPLEX,
+    #                 1,
+    #                 (0,0,0))
+
+
+
+    #callbacks for sliders
+    def set_h_lb(self, val):
+        """ set hue lower bound """
+        self.hsv_lb[0] = val
+
+    def set_s_lb(self, val):
+        """ set saturation lower bound """
+        self.hsv_lb[1] = val
+
+    def set_v_lb(self, val):
+        """ set value lower bound """
+        self.hsv_lb[2] = val
+
+    def set_h_ub(self, val):
+        """ set hue upper bound """
+        self.hsv_ub[0] = val
+
+    def set_s_ub(self, val):
+        """ set saturation upper bound """
+        self.hsv_ub[1] = val
+
+    def set_v_ub(self, val):
+        """ set value upper bound """
+        self.hsv_ub[2] = val
 
     def run(self):
         """ The main run loop"""
         r = rospy.Rate(10)
         while not rospy.is_shutdown():
+            
+            #code for finding hsv at mousepoint
+            # if not self.image_info_window is None:
+            #     cv2.imshow('image_info', self.image_info_window)
+            #     cv2.waitKey(5)
+
             if not self.cv_image is None:
-                print "here"
+                # print "here"
+
                 # creates a window and displays the image for X milliseconds
+                # cv2.imshow('video_window', self.cv_image)
                 cv2.imshow('video_window', self.cv_image)
+                # cv2.imshow('video_window2', self.cropped_sign)
                 cv2.waitKey(5)
             r.sleep()
 
--- scripts/template_matcher.py
+++ scripts/template_matcher.py
@@ -1,5 +1,6 @@
 import cv2
-
+import math
+import numpy as np
 """
 This code determines which of a set of template images matches
 an input image the best using the SIFT algorithm
@@ -11,6 +12,7 @@ class TemplateMatcher(object):
         self.signs = {} #maps keys to the template images
         self.kps = {} #maps keys to the keypoints of the template images
         self.descs = {} #maps keys to the descriptors of the template images
+        # self.ransac_thresh
         if cv2.__version__=='3.1.0-dev':
             self.sift = cv2.xfeatures2d.SIFT_create()
         else:
@@ -18,9 +20,14 @@ class TemplateMatcher(object):
 
         # for potential tweaking
         self.min_match_count = min_match_count
-        self.good_thresh = good_thresh #use for keypoint threshold
+        self.ransac_thresh = good_thresh #use for keypoint threshold
+        for k, filename in images.iteritems():
+            # load template sign images as grayscale
+            self.signs[k] = cv2.imread(filename,0)
 
-        #TODO: precompute keypoints for template images
+            # precompute keypoints and descriptors for the template sign 
+            self.kps[k], self.descs[k] = self.sift.detectAndCompute(self.signs[k],None)
+
 
 
     def predict(self, img):
@@ -29,25 +36,32 @@ class TemplateMatcher(object):
         returns a dictionary, keys being signs, values being confidences
         """
         visual_diff = {}
+        kp = None
+        des = None
+        
 
-        # TODO: get keypoints and descriptors from input image using SIFT
-        #       store keypoints in variable kp and descriptors in des
+        kp, des = self.sift.detectAndCompute(img,None)
+
+
 
         for k in self.signs.keys():
-            #cycle trough templage images (k) and get the image differences
+            #cycle trough template images (k) and get the image differences
             visual_diff[k] = self._compute_prediction(k, img, kp, des)
 
         if visual_diff:
-            pass
-            # TODO: convert difference between images (from visual_diff)
-            #       to confidence values (stored in template_confidence)
+            # print self.signs.keys()
+            temp = []
+            for k in self.signs.keys():
+                temp.append(visual_diff[k])
+
+            total = sum(temp)
+
+            template_confidence = {k: (visual_diff[k]/total) for k in self.signs.keys()}
 
         else: # if visual diff was not computed (bad crop, homography could not be computed)
             # set 0 confidence for all signs
             template_confidence = {k: 0 for k in self.signs.keys()}
             
-        #TODO: delete line below once the if statement is written
-        template_confidence = {k: 0 for k in self.signs.keys()}
 
         return template_confidence
 
@@ -58,16 +72,63 @@ class TemplateMatcher(object):
         k: template image for comparison, img: scene image
         kp: keypoints from scene image,   des: descriptors from scene image
         """
-
-        # TODO: find corresponding points in the input image and the templae image
-        #       put keypoints from template image in template_pts
-        #       put corresponding keypoints from input image in img_pts
+        #for every key point on template, compare it to all input key points and pick the closest to match with
 
 
-        #TODO: change img to img_T once you do the homography transform
-        visual_diff = compare_images(img, self.signs[k])
+        template_pts = []
+        img_pts = []
+
+        for temp in self.kps[k]:
+            lowest_dist = 1000000000
+            curr_temp = None
+            curr_scene = None
+            for keyp in kp:
+                x1 = temp.pt[0]
+                x2 = keyp.pt[0]
+                y1 = temp.pt[1]
+                y2 = keyp.pt[1]
+                distance = math.sqrt(((x2-x1)**2)+(y2-y1)**2)
+                if distance < lowest_dist:
+                    curr_temp = temp
+                    curr_scene = keyp
+
+
+            template_pts.append([curr_temp.pt[0],curr_temp.pt[1]])
+            img_pts.append([curr_scene.pt[0],curr_scene.pt[1]])
+
+        template_pts = np.array(template_pts)
+        img_pts = np.array(img_pts)
+        # Transform input image so that it matches the template image as well as possible
+        M, mask = cv2.findHomography(img_pts[:10], template_pts[:10], cv2.RANSAC, self.ransac_thresh)
+        img_t = cv2.warpPerspective(img, M, self.signs[k].shape[::-1])
+
+        visual_diff = compare_images(img_t, self.signs[k])
         return visual_diff
 # end of TemplateMatcher class
 
 def compare_images(img1, img2):
-    return 0
+
+    err = np.sum((img1.astype("float") - img2.astype("float")) ** 2)
+    err /= float(img1.shape[0] * img2.shape[1])
+    return err
+
+
+
+if __name__ == '__main__':
+    images = {
+        "left": '../images/leftturn_box_small.png',
+        "right": '../images/rightturn_box_small.png',
+        "uturn": '../images/uturn_box_small.png'
+        }
+
+    tm = TemplateMatcher(images)
+    scenes = [
+    "../images/uturn_scene.jpg",
+    "../images/leftturn_scene.jpg",
+    "../images/rightturn_scene.jpg"]
+
+for filename in scenes:
+    scene_img = cv2.imread(filename, 0)
+    pred = tm.predict(scene_img)
+    print filename.split('/')[-1]
+    print pred
