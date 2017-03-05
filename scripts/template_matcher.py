c4cfec75bee1791eb7318b1fb0466fbd059b0625
Jonah Spear <Jonahspear512@gmail.com>
2017-03-05 16:55:20 -0500

    got template matching working

--- scripts/template_matcher.py
+++ scripts/template_matcher.py
@@ -1,7 +1,11 @@
+#!/usr/bin/env python
+
 import cv2
+import numpy as np
+import matplotlib.pyplot as plt
 
 """
-This code determines which of a set of template images matches
+This code determines which of a set of template images matches 
 an input image the best using the SIFT algorithm
 """
 
@@ -20,8 +24,12 @@ class TemplateMatcher(object):
         self.min_match_count = min_match_count
         self.good_thresh = good_thresh #use for keypoint threshold
 
-        #TODO: precompute keypoints for template images
+        for k, filename in images.iteritems():
+            # load template sign images as greyscale
+            self.signs[k] = cv2.imread(filename, 0)
 
+            # precompute keypoints and descriptors for the template sign
+            self.kps[k], self.descs[k] = self.sift.detectAndCompute(self.signs[k], None)
 
     def predict(self, img):
         """
@@ -30,25 +38,20 @@ class TemplateMatcher(object):
         """
         visual_diff = {}
 
-        # TODO: get keypoints and descriptors from input image using SIFT
-        #       store keypoints in variable kp and descriptors in des
+        # get keypoints and descriptors from image using SIFT
+        kp, des = self.sift.detectAndCompute(img, None)
 
         for k in self.signs.keys():
             #cycle trough templage images (k) and get the image differences
             visual_diff[k] = self._compute_prediction(k, img, kp, des)
 
         if visual_diff:
-            pass
-            # TODO: convert difference between images (from visual_diff)
-            #       to confidence values (stored in template_confidence)
+            template_confidence = {k: visual_diff[k]**-1 for k in self.signs.keys()}
 
         else: # if visual diff was not computed (bad crop, homography could not be computed)
             # set 0 confidence for all signs
             template_confidence = {k: 0 for k in self.signs.keys()}
             
-        #TODO: delete line below once the if statement is written
-        template_confidence = {k: 0 for k in self.signs.keys()}
-
         return template_confidence
 
 
@@ -59,15 +62,111 @@ class TemplateMatcher(object):
         kp: keypoints from scene image,   des: descriptors from scene image
         """
 
-        # TODO: find corresponding points in the input image and the templae image
-        #       put keypoints from template image in template_pts
-        #       put corresponding keypoints from input image in img_pts
+        # Transform input image so that it matches the template image as well as possible
+        
 
+        # Match the points in our template and scene
+        bf = cv2.BFMatcher(cv2.NORM_L2, crossCheck=True)
+        matches = bf.match(self.descs[k], des)
+        # Sort them according to distance
+        matches = sorted(matches, key=lambda x:x.distance)
 
-        #TODO: change img to img_T once you do the homography transform
-        visual_diff = compare_images(img, self.signs[k])
+        kp1_match = [kp[match.trainIdx] for match in matches]
+        kp2_match = [self.kps[k][match.queryIdx] for match in matches]
+        scene_pts = np.array([keypoint.pt for keypoint in kp1_match])
+        template_pts = np.array([keypoint.pt for keypoint in kp2_match])
+
+        # draw_matches(self.signs[k], self.kps[k], img, kp, matches)
+
+        M, mask = cv2.findHomography(scene_pts, template_pts, cv2.RANSAC, self.good_thresh)
+        img_T = cv2.warpPerspective(img, M, self.signs[k].shape[::-1])
+
+        visual_diff = compare_images(img_T, self.signs[k])
         return visual_diff
 # end of TemplateMatcher class
 
 def compare_images(img1, img2):
-    return 0
+    """
+    Get euclidean distance between two images
+    """
+    img1_norm = cv2.normalize(img1, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+    img2_norm = cv2.normalize(img2, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+    diff_squared = (img1_norm - img2_norm)**2
+    dist = np.sum(diff_squared[:])
+    return dist
+
+def draw_matches(img1, kp1, img2, kp2, matches, color=None): 
+    """Draws lines between matching keypoints of two images.  
+    Keypoints not in a matching pair are not drawn.
+    Places the images side by side in a new image and draws circles 
+    around each keypoint, with line segments connecting matching pairs.
+    You can tweak the r, thickness, and figsize values as needed.
+    Args:
+        img1: An openCV image ndarray in a grayscale or color format.
+        kp1: A list of cv2.KeyPoint objects for img1.
+        img2: An openCV image ndarray of the same format and with the same 
+        element type as img1.
+        kp2: A list of cv2.KeyPoint objects for img2.
+        matches: A list of DMatch objects whose trainIdx attribute refers to 
+        img1 keypoints and whose queryIdx attribute refers to img2 keypoints.
+        color: The color of the circles and connecting lines drawn on the images.  
+        A 3-tuple for color images, a scalar for grayscale images.  If None, these
+        values are randomly generated.  
+    """
+    # We're drawing them side by side.  Get dimensions accordingly.
+    # Handle both color and grayscale images.
+    
+    if len(img1.shape) == 3:
+        new_shape = (max(img1.shape[0], img2.shape[0]), img1.shape[1]+img2.shape[1], img1.shape[2])
+    elif len(img1.shape) == 2:
+        new_shape = (max(img1.shape[0], img2.shape[0]), img1.shape[1]+img2.shape[1])
+    new_img = np.zeros(new_shape, type(img1.flat[0]))  
+    # Place images onto the new image.
+    new_img[0:img1.shape[0],0:img1.shape[1]] = img1
+    new_img[0:img2.shape[0],img1.shape[1]:img1.shape[1]+img2.shape[1]] = img2
+    # Draw lines between matches.  Make sure to offset kp coords in second image appropriately.
+    
+    r = 2
+    thickness = 2
+    if color:
+        c = color
+    for m in matches:
+        # Generate random color for RGB/BGR and grayscale images as needed.
+        if not color: 
+            c = np.random.randint(0,256,3) if len(img1.shape) == 3 else np.random.randint(0,256)
+        # So the keypoint locs are stored as a tuple of floats.  cv2.line(), like most other things,
+        # wants locs as a tuple of ints.
+        end1 = tuple(np.round(kp1[m.trainIdx].pt).astype(int))
+        try:
+            end2 = tuple(np.round(kp2[m.queryIdx].pt).astype(int) + np.array([img1.shape[1], 0]))
+        except IndexError:
+            import pdb
+            pdb.set_trace()
+        cv2.line(new_img, end1, end2, c, thickness)
+        cv2.circle(new_img, end1, r, c, thickness)
+        cv2.circle(new_img, end2, r, c, thickness)
+    
+    plt.figure(figsize=(15,15))
+    plt.imshow(new_img)
+    plt.show()
+
+if __name__ == '__main__':
+    images = {
+        "left": '../images/leftturn_box_small.png',
+        "right": '../images/rightturn_box_small.png',
+        "uturn": '../images/uturn_box_small.png'
+        }
+
+    tm = TemplateMatcher(images)
+
+    scenes = [
+        "../images/uturn_scene.jpg",
+        "../images/leftturn_scene.jpg",
+        "../images/rightturn_scene.jpg"
+    ]
+
+    for filename in scenes:
+        scene_img = cv2.imread(filename, 0)
+        pred = tm.predict(scene_img)
+        print filename.split('/')[-1]
+        print pred
