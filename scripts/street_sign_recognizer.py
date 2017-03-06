369512b0d8d39b296a30afe78761a77bf8eb3b18
Riley Chapman <riley.chapman@students.olin.edu>
2017-03-06 13:16:45 -0500

    hsv image conversion and thresholding set

--- scripts/street_sign_recognizer.py
+++ scripts/street_sign_recognizer.py
@@ -17,14 +17,61 @@ class StreetSignRecognizer(object):
         """ Initialize the street sign reocgnizer """
         rospy.init_node('street_sign_recognizer')
         self.cv_image = None                        # the latest image from the camera
+        self.hsv_image = None                       # image converted to HSV
+        self.threshold_image = None                 # binary threshold image
+        self.grid_cell = None
         self.bridge = CvBridge()                    # used to convert ROS messages to OpenCV
         cv2.namedWindow('video_window')
         rospy.Subscriber("/camera/image_raw", Image, self.process_image)
+        self.image_info_window = None   #setup mouse callback
+        cv2.setMouseCallback('video_window', self.process_mouse_event)
+        self.grid_cell_w = 64*3 #setup grid
+        self.grid_cell_h = 48*3
+
+        #setup OpenCV trackbars
+        cv2.namedWindow('threshold_image')
+        self.hsv_lb = np.array([22, 173, 185]) # hsv lower bound
+        cv2.createTrackbar('H lb', 'threshold_image', self.hsv_lb[0], 255, self.set_h_lb)
+        cv2.createTrackbar('S lb', 'threshold_image', self.hsv_lb[1], 255, self.set_s_lb)
+        cv2.createTrackbar('V lb', 'threshold_image', self.hsv_lb[2], 255, self.set_v_lb)
+        self.hsv_ub = np.array([32, 255, 255]) # hsv upper bound
+        cv2.createTrackbar('H ub', 'threshold_image', self.hsv_ub[0], 255, self.set_h_ub)
+        cv2.createTrackbar('S ub', 'threshold_image', self.hsv_ub[1], 255, self.set_s_ub)
+        cv2.createTrackbar('V ub', 'threshold_image', self.hsv_ub[2], 255, self.set_v_ub)
+
+    def set_h_lb(self, val):
+        """ set hue lower bound """
+        self.hsv_lb[0] = val
+
+    def set_s_lb(self, val):
+        """ set saturation lower bound """
+        self.hsv_lb[1] = val
+
+    def set_v_lb(self, val):
+        """ set value lower bound """
+        self.hsv_lb[2] = val
+
+    def set_h_ub(self, val):
+        """ set hue upper bound """
+        self.hsv_ub[0] = val
+
+    def set_s_ub(self, val):
+        """ set saturation upper bound """
+        self.hsv_ub[1] = val
+
+    def set_v_ub(self, val):
+        """ set value upper bound """
+        self.hsv_ub[2] = val
 
     def process_image(self, msg):
         """ Process image messages from ROS and stash them in an attribute
             called cv_image for subsequent processing """
         self.cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
+        self.hsv_image = cv2.cvtColor(self.cv_image, cv2.COLOR_BGR2HSV)
+        self.threshold_image = cv2.inRange(self.hsv_image, self.hsv_lb, self.hsv_ub)
+
+        self.grid_cell = self.hsv_image[self.grid_cell_h:2*self.grid_cell_h,
+                    self.grid_cell_w:2*self.grid_cell_w] 
 
         left_top, right_bottom = self.sign_bounding_box()
         left, top = left_top
@@ -48,14 +95,40 @@ class StreetSignRecognizer(object):
         right_bottom = (400, 400)
         return left_top, right_bottom
 
+    def process_mouse_event(self, event, x,y,flags,param):
+        """ Process mouse events so that you can see the color values associated
+            with a particular pixel in the camera images """
+        self.image_info_window = 255*np.ones((500,500,3))
+
+        # show hsv values
+        cv2.putText(self.image_info_window,
+                    'Color (h=%d,s=%d,v=%d)' % (self.hsv_image[y,x,0], self.hsv_image[y,x,1], self.hsv_image[y,x,2]),
+                    (5,50), # 5 = x, 50 = y
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    1,
+                    (0,0,0))
+
+        # show bgr values
+        # cv2.putText(self.image_info_window,
+        #             'Color (b=%d,g=%d,r=%d)' % (self.cv_image[y,x,0], self.cv_image[y,x,1], self.cv_image[y,x,2]),
+        #             (5,100),
+        #             cv2.FONT_HERSHEY_SIMPLEX,
+        #             1,
+        #             (0,0,0))
+
+
     def run(self):
         """ The main run loop"""
         r = rospy.Rate(10)
         while not rospy.is_shutdown():
             if not self.cv_image is None:
-                print "here"
+                #print "here"
                 # creates a window and displays the image for X milliseconds
-                cv2.imshow('video_window', self.cv_image)
+                cv2.imshow('video_window', self.grid_cell)
+                cv2.imshow('threshold_image', self.threshold_image)
+                cv2.waitKey(5)
+            if not self.image_info_window is None:
+                cv2.imshow('image_info', self.image_info_window)
                 cv2.waitKey(5)
             r.sleep()
 
