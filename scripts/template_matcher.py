fb550a97350ee01b24ed63a8760e6c79f58ecf7c
Bonnie Ishiguro <bgishiguro@gmail.com>
2017-03-06 12:59:53 -0500

    visual diff confidence value

--- scripts/template_matcher.py
+++ scripts/template_matcher.py
@@ -16,10 +16,9 @@ class TemplateMatcher(object):
         else:
             self.sift = cv2.SIFT() #initialize SIFT to be used for image matching
 
-        # for potential tweaking
         self.min_match_count = min_match_count
-        self.good_thresh = good_thresh #use for keypoint threshold
-        self.ransac_thres = 0.8
+        self.good_thresh = good_thresh #use for keypoint threshold        
+        self.ransac_thres = 0.8 # FIXME: value?
 
         # precompute keypoints for template images
         for k, filename in images.iteritems():
@@ -37,7 +36,9 @@ class TemplateMatcher(object):
         Uses gather predictions to get visual diffs of the image to each template
         returns a dictionary, keys being signs, values being confidences
         """
+        
         visual_diff = {}
+        template_confidence = {}
 
         # get keypoints and descriptors from input image using SIFT
         # store keypoints in variable kp and descriptors in des
@@ -45,19 +46,26 @@ class TemplateMatcher(object):
 
         for k in self.signs.keys():
             #cycle through template images (k) and get the image differences
-            visual_diff[k] = self._compute_prediction(k, img, kp, des)
+            visual_diff[k] = 1.2 # TODO: replace with self._compute_prediction(k, img, kp, des)
 
         if visual_diff:
-            print visual_diff
-            # TODO: convert difference between images (from visual_diff)
-            #       to confidence values (stored in template_confidence)
+            # convert difference between images (from visual_diff)
+            # to confidence values (stored in template_confidence)
+            template_confidence_sum = 0.0
+            for k in visual_diff:
+                template_confidence[k] = 1/visual_diff[k]
+                template_confidence_sum += template_confidence[k]
+                print "template confidence", template_confidence[k]
+            
+            # normalize confidence values
+            for k in template_confidence:
+                template_confidence[k] /= template_confidence_sum
 
         else: # if visual diff was not computed (bad crop, homography could not be computed)
             # set 0 confidence for all signs
             template_confidence = {k: 0 for k in self.signs.keys()}
             
-        #TODO: delete line below once the if statement is written
-        template_confidence = {k: 0 for k in self.signs.keys()}
+        print "template_confidence_sum", template_confidence_sum
 
         return template_confidence
 
@@ -69,17 +77,17 @@ class TemplateMatcher(object):
         kp: keypoints from scene image,   des: descriptors from scene image
         """
 
-        # TODO: find corresponding points in the input image and the templae image
+        # TODO: find corresponding points in the input image and the template image
         #       put keypoints from template image in template_pts
         #       put corresponding keypoints from input image in img_pts
 
-        # Transform input image so that it matches the template image as well as possible
-        
-        # print img
+        # kp: input keypoints
+        # self.kps: template keypoints
 
         # img_pts = 
         # template_pts = 
 
+        # Transform input image so that it matches the template image as well as possible
         M, mask = cv2.findHomography(img_pts, template_pts, cv2.RANSAC, self.ransac_thresh)
         img_T = cv2.warpPerspective(img, M, self.signs[k].shape[::-1])
 
@@ -88,4 +96,11 @@ class TemplateMatcher(object):
 # end of TemplateMatcher class
 
 def compare_images(img1, img2):
-    return 0
+    # normalize images: (each_element - mean)/standard_dev
+    img1_norm = (img1 - np.mean(img1)) / np.std(img1)
+    img2_norm = (img2 - np.mean(img2)) / np.std(img2)
+
+    diff = np.substract(img1, img2) # absolute value?
+    # matrix norm of difference
+    norm_diff = np.linalg.norm(diff)
+    return norm_diff
