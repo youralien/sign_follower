9d4337d154519b9ab858a64e92dea91afa90424f
Shane Kelly <skelly1@lappy-14.04>
2017-03-06 01:28:09 -0500

    ADD: finish sign detection

--- scripts/street_sign_recognizer.py
+++ scripts/street_sign_recognizer.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python 
 
 """ This is a script that walks through some of the basics of working with images
     with opencv in ROS. """
@@ -8,6 +8,7 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import cv2
 import numpy as np
+from template_matcher import TemplateMatcher
 
 class StreetSignRecognizer(object):
     """ This robot should recognize street signs """
@@ -16,12 +17,28 @@ class StreetSignRecognizer(object):
     def __init__(self):
         """ Initialize the street sign reocgnizer """
         rospy.init_node('street_sign_recognizer')
+
+        # Create TemplateMatcher object with template images
+        self.template_matcher = TemplateMatcher(
+            {
+                "left":'../images/leftturn_box_small.png',
+                "right":'../images/rightturn_box_small.png',
+                "uturn":'../images/uturn_box_small.png'
+            }
+        )
+
         self.bgr_image = None                        # the latest image from the camera
         self.hsv_image = None
         self.filt_image = None
+        self.cropped_sign_grayscale = None
+
         self.bridge = CvBridge()                    # used to convert ROS messages to OpenCV
+        
+        # Create windows to view video streams
         cv2.namedWindow('video_window')
         cv2.namedWindow('filt_window')
+        cv2.namedWindow('cropped_grayscale_window')
+        cv2.namedWindow('img_T_window')
 
         # Create sliders to control image color filtering
         cv2.namedWindow('threshold_image')
@@ -47,6 +64,7 @@ class StreetSignRecognizer(object):
         cv2.setTrackbarPos('V upper bound', 'threshold_image', self.v_upper_bound)
         cv2.setTrackbarPos('Blur amount', 'threshold_image', self.blur_amount)
 
+        # Subscribe to ROS image stream
         rospy.Subscriber("/camera/image_raw", Image, self.process_image)
 
     def set_h_lower_bound(self, val):
@@ -81,20 +99,30 @@ class StreetSignRecognizer(object):
     def process_image(self, msg):
         """ Process image messages from ROS and stash them in an attribute
             called cv_image for subsequent processing """
+        # Convert ROS image stream to opencv image stream
         self.bgr_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
+
+        # Gaussian blur the image to low-pass contents
         self.blurred_bgr_image = cv2.GaussianBlur(self.bgr_image, \
             (self.blur_amount, self.blur_amount), 0)
+
+        # Shift to HSV image and filter for color of traffic signs
         self.hsv_image = cv2.cvtColor(self.blurred_bgr_image, cv2.COLOR_BGR2HSV)
-        self.filt_image = cv2.inRange(self.hsv_image, (self.h_lower_bound, self.s_lower_bound, \
-            self.v_lower_bound), (self.h_upper_bound, self.s_upper_bound, self.v_upper_bound))
+        self.filt_image = cv2.inRange(self.hsv_image, \
+            (self.h_lower_bound, self.s_lower_bound, self.v_lower_bound),
+            (self.h_upper_bound, self.s_upper_bound, self.v_upper_bound))
+
+        # Define a bounding box around the detected color
         left_top, right_bottom = self.sign_bounding_box()
         left, top = left_top
         right, bottom = right_bottom
-
-        # crop bounding box region of interest
         cropped_sign = self.bgr_image[top:bottom, left:right]
 
-        # draw bounding box rectangle
+        # Convert bounded image to grayscale and detect type of traffic sign
+        self.cropped_sign_grayscale = cv2.cvtColor(cropped_sign, cv2.COLOR_BGR2GRAY)
+        print self.template_matcher.predict(self.cropped_sign_grayscale)
+
+        # Draw visual bounding box on bgr_image
         cv2.rectangle(self.bgr_image, left_top, right_bottom, color=(0, 0, 255), thickness=5)
 
     def sign_bounding_box(self):
@@ -104,44 +132,36 @@ class StreetSignRecognizer(object):
         (left_top, right_bottom) where left_top and right_bottom are tuples of (x_pixel, y_pixel)
             defining topleft and bottomright corners of the bounding box
         """
+        # Find contours based on the binary filtered image
         contours, hierarchy = cv2.findContours(self.filt_image, cv2.RETR_TREE, \
            cv2.CHAIN_APPROX_SIMPLE)
-        x, y, w, h = cv2.boundingRect(contours[0])
-        left_top = (x, y)
-        right_bottom = (x+w, y+h)
-        return left_top, right_bottom
+
+        # Define bounding rectangle around contour
+        if (contours):
+            x, y, w, h = cv2.boundingRect(contours[0])
+            left_top = (x, y)
+            right_bottom = (x+w, y+h)
+            return left_top, right_bottom
+        else:
+            # If no contours were found
+            return 0, 0
 
     def run(self):
         """ The main run loop"""
         r = rospy.Rate(10)
         while not rospy.is_shutdown():
-            if (not self.bgr_image is None) and (not self.filt_image is None):
-                # creates a window and displays the image for X milliseconds
+            # Create windows to show all specified image streams
+            if (not self.bgr_image is None):
                 cv2.imshow('video_window', self.bgr_image)
+            if (not self.filt_image is None):
                 cv2.imshow('filt_window', self.filt_image)
-                cv2.waitKey(5)
+            if (not self.cropped_sign_grayscale is None):
+                cv2.imshow('cropped_grayscale_window', self.cropped_sign_grayscale)
+            if (not self.template_matcher.img_T is None):
+                cv2.imshow('img_T_window', self.template_matcher.img_T)
+            cv2.waitKey(5)
             r.sleep()
 
 if __name__ == '__main__':
-    images = {
-        "left": '../images/leftturn_box_small.png',
-        "right": '../images/rightturn_box_small.png',
-        "uturn": '../images/uturn_box_small.png'
-        }
-
-    tm = TemplateMatcher(images)
-
-    scenes = [
-        "../images/uturn_scene.jpg",
-        "../images/leftturn_scene.jpg",
-        "../images/rightturn_scene.jpg"
-    ]
-
-    for filename in scenes:
-        scene_img = cv2.imread(filename, 0)
-        pred = tm.predict(scene_img)
-        print filename.split('/')[-1]
-        print pred 
-
     node = StreetSignRecognizer()
     node.run()
--- scripts/template_matcher.py
+++ scripts/template_matcher.py
@@ -1,13 +1,16 @@
-import cv2
+#!/usr/bin/env python 
 
 """
 This code determines which of a set of template images matches
 an input image the best using the SIFT algorithm
 """
 
+import cv2
+import numpy as np
+
 class TemplateMatcher(object):
 
-    def __init__ (self, images, min_match_count=10, good_thresh=0.7):
+    def __init__ (self, images, min_match_count=10, good_thresh=0.5):
         self.signs = {} #maps keys to the template images
         self.kps = {} #maps keys to the keypoints of the template images
         self.descs = {} #maps keys to the descriptors of the template images
@@ -20,12 +23,15 @@ class TemplateMatcher(object):
         self.min_match_count = min_match_count
         self.good_thresh = good_thresh #use for keypoint threshold
 
-        #for k, filename in images.iteritems():
-        #    # load template sign images as grayscale
-        #    self.signs[k] = cv2.imread(filename,0)
+        self.img_T = None
+        self.ransac_thresh = 5.0
 
-        #    # precompute keypoints and descriptors for the template sign 
-        #    self.kps[k], self.descs[k] = self.sift.detectAndCompute(self.signs[k],None)
+        for k, filename in images.iteritems():
+            # load template sign images as grayscale
+            self.signs[k] = cv2.imread(filename,0)
+
+            # precompute keypoints and descriptors for the template sign 
+            self.kps[k], self.descs[k] = self.sift.detectAndCompute(self.signs[k],None)
 
 
     def predict(self, img):
@@ -34,26 +40,27 @@ class TemplateMatcher(object):
         returns a dictionary, keys being signs, values being confidences
         """
         visual_diff = {}
+        
+        # compute keypoint and descriptor for img
+        kp, des = self.sift.detectAndCompute(img,None)
 
-        # TODO: get keypoints and descriptors from input image using SIFT
-        #       store keypoints in variable kp and descriptors in des
+        if (des != None and img != None):
+            for k in self.signs.keys():
+                #cycle trough templage images (k) and get the image differences
+                visual_diff[k] = self._compute_prediction(k, img, kp, des)
 
-        for k in self.signs.keys():
-            #cycle trough templage images (k) and get the image differences
-            visual_diff[k] = self._compute_prediction(k, img, kp, des)
-
+        # Find template_confidence by inverting and normalizing visual_diff
         if visual_diff:
-            pass
-            # TODO: convert difference between images (from visual_diff)
-            #       to confidence values (stored in template_confidence)
+            template_confidence = visual_diff
+            total = sum(template_confidence.values())
+            for k in template_confidence:
+                if (template_confidence[k] != 0):
+                    template_confidence[k] /= total
 
         else: # if visual diff was not computed (bad crop, homography could not be computed)
             # set 0 confidence for all signs
             template_confidence = {k: 0 for k in self.signs.keys()}
             
-        #TODO: delete line below once the if statement is written
-        template_confidence = {k: 0 for k in self.signs.keys()}
-
         return template_confidence
 
 
@@ -64,34 +71,98 @@ class TemplateMatcher(object):
         kp: keypoints from scene image,   des: descriptors from scene image
         """
 
-        # TODO: find corresponding points in the input image and the templae image
-        #       put keypoints from template image in template_pts
-        #       put corresponding keypoints from input image in img_pts
+        img_pts = []
+        template_pts = []
+        closest_dists = []
 
+        # Quantify the similarity between each descriptor in the streamed image
+        # and each descriptor in the specified template image. Only keep track
+        # of the two closest template descriptor matches for each stream image
+        # descriptor.
+        for img_des_num, img_des in enumerate(des):
+            closest_dists.append([
+                {"temp_des_num":0, "dist":1000000},
+                {"temp_des_num":0, "dist":999999}
+            ])
+            for temp_des_num, temp_des in enumerate(self.descs[k]):
+                dist = np.linalg.norm(img_des-temp_des, 2)
+                # Keep dist if closer than one of closest distances
+                if (dist < closest_dists[img_des_num][0]["dist"]):
+                    if (dist < closest_dists[img_des_num][1]["dist"]):
+                        closest_dists[img_des_num][0] = \
+                            closest_dists[img_des_num][1]
+                        closest_dists[img_des_num][1] = \
+                            {"temp_des_num":temp_des_num, "dist":dist}
+                    else:
+                        closest_dists[img_des_num][0] = \
+                            {"temp_des_num":temp_des_num, "dist":dist}
 
-        #TODO: change img to img_T once you do the homography transform
-        visual_diff = compare_images(img, self.signs[k])
+        # Go through the collected closest distances between stream image
+        # descriptors and template image descriptors. If any match passes
+        # the good threshold, then add it to the appropriate list.
+        for img_des_num, close_dist in enumerate(closest_dists):
+            if (close_dist[1]["dist"] < self.good_thresh*close_dist[0]["dist"]):
+                img_pts.append(kp[img_des_num].pt)
+                template_pts.append(self.kps[k][close_dist[1]["temp_des_num"]].pt)
+
+        img_pts = np.asarray(img_pts)
+        template_pts = np.asarray(template_pts)
+                
+        # Transform input image so that it matches the template image as well as possible
+        if (len(img_pts) > 3):
+            M, mask = cv2.findHomography(img_pts, template_pts, cv2.RANSAC, self.ransac_thresh)
+            self.img_T = cv2.warpPerspective(img, M, self.signs[k].shape[::-1])
+
+            visual_diff = compare_images(self.img_T, self.signs[k])
+        else:
+            # Image could not be warped, return a a large visual_diff
+            visual_diff = 100000000000
+
         return visual_diff
-# end of TemplateMatcher class
 
-#def compare_images(img1, img2):
-#    return 0
+
+def compare_images(img1, img2):
+    """
+    Return value for how similar two images are after being normalized. The
+    returned value will be between 0 and 255, where 0 means the images are
+    identical.
+    img1: first image to compare
+    img2: second image to compare
+    """
+
+    # Find mean and standard deviation of each image
+    img1_mean = np.mean(img1)
+    img1_std = np.std(img1)
+    img2_mean = np.mean(img2)
+    img2_std = np.std(img2)
+
+    # Normalize the images to remove lighting differences
+    img1 = img1 - img1_mean / img1_std
+    img2 = img2 - img2_mean / img2_std
+
+    # Find the absolute difference between the two images
+    diff = abs(img1-img2)
+    diff_mean = np.mean(diff)
+
+    return diff_mean
 
 if __name__ == '__main__':
-    print "hello"
+    images = {
+        "left":'../images/leftturn_box_small.png',
+        "right":'../images/rightturn_box_small.png',
+        "uturn":'../images/uturn_box_small.png'
+    }
 
-    #images = {"left":'../images/leftturn_box_small.png', "right":'../images/rightturn_box_small.png', "uturn":'../images/uturn_box_small.png'}
+    tm = TemplateMatcher(images)
 
-    #tm = TemplateMatcher(images)
+    scenes = [
+        "../images/uturn_scene.jpg",
+        "../images/leftturn_scene.jpg",
+        "../images/rightturn_scene.jpg"
+    ]
 
-    #scenes = [
-    #    "../images/uturn_scene.jpg",
-    #    "../images/leftturn_scene.jpg",
-    #    "../images/rightturn_scene.jpg"
-    #]
-
-    #for filename in scenes:
-    #    scene_img = cv2.imread(filename, 0)
-    #    pred = tm.predict(scene_img)
-    #    print filename.split('/')[-1]
-    #    print pred
+    for filename in scenes:
+        scene_img = cv2.imread(filename, 0)
+        pred = tm.predict(scene_img)
+        print filename.split('/')[-1]
+        print pred
