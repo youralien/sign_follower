8a01b341f3b0f5407ab9ed1c44cdc6136c4ebdc2
SeanCCarter <seancarter26@gmail.com>
2017-03-07 11:31:25 -0500

    Finished template matching on the street signs

--- scripts/street_sign_recognizer.py
+++ scripts/street_sign_recognizer.py
@@ -8,10 +8,10 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import cv2
 import numpy as np
+import template_matcher
 
 class StreetSignRecognizer(object):
     """ This robot should recognize street signs """
-
 
     def __init__(self):
         """ Initialize the street sign reocgnizer """
@@ -20,42 +20,18 @@ class StreetSignRecognizer(object):
         self.bridge = CvBridge()                    # used to convert ROS messages to OpenCV
         cv2.namedWindow('video_window')
         rospy.Subscriber("/camera/image_raw", Image, self.process_image)
-        cv2.namedWindow('binary_window')
-
-        self.hue_lower_bound = 0
-        self.hue_upper_bound = 50
-        self.saturation_lower_bound = 175
-        self.saturation_upper_bound = 255
-        cv2.createTrackbar('hue lower bound', 'binary_window', 0, 255, self.set_hue_lower_bound)
-        cv2.createTrackbar('hue upper bound', 'binary_window', 0, 255, self.set_hue_upper_bound)
-        cv2.createTrackbar('saturation lower bound', 'binary_window', 0, 255, self.set_saturation_lower_bound)
-        cv2.createTrackbar('saturation upper bound', 'binary_window', 0, 255, self.set_saturation_upper_bound)
-
-    def set_hue_lower_bound(self, val):
-        """ A callback function to handle the OpenCV slider to select the red lower bound """
-        self.hue_lower_bound = val
-
-    def set_hue_upper_bound(self, val):
-        """ A callback function to handle the OpenCV slider to select the red lower bound """
-        self.hue_upper_bound = val
-
-    def set_saturation_lower_bound(self, val):
-        """ A callback function to handle the OpenCV slider to select the red lower bound """
-        self.saturation_lower_bound = val
-
-    def set_saturation_upper_bound(self, val):
-        """ A callback function to handle the OpenCV slider to select the red lower bound """
-        self.saturation_upper_bound = val
 
     def process_image(self, msg):
         """ Process image messages from ROS and stash them in an attribute
             called cv_image for subsequent processing """
+
+        #Connecting to the image publisher, then converting to HSV format
+        #Then using HSV to identify the yellow in the sign (representing in binary_image)
         self.cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
         self.hsv_image = cv2.cvtColor(self.cv_image, cv2.COLOR_BGR2HSV)
-        self.binary_image = cv2.inRange(self.cv_image,
-            (self.hue_lower_bound,self.saturation_lower_bound,100),
-            (self.hue_upper_bound,self.saturation_upper_bound,255))
+        self.binary_image = cv2.inRange(self.cv_image,(0,175,100),(50,255,255))
 
+        #Find the edges of the sign, for cropping
         left_top, right_bottom = self.sign_bounding_box()
         left, top = left_top
         right, bottom = right_bottom
@@ -73,14 +49,16 @@ class StreetSignRecognizer(object):
         (left_top, right_bottom) where left_top and right_bottom are tuples of (x_pixel, y_pixel)
             defining topleft and bottomright corners of the bounding box
         """
-        # _, contours, hierarchy = cv2.findContours(self.binary_image, 1, 2)
-        #image_copy = self.binary_image
+        #Create list of points where the sign is
         points = np.transpose(np.nonzero(self.binary_image))
+        #Get descriptors of boundry box
         x,y,w,h = cv2.boundingRect(points)
+
+        #Identify points based on what was found
+        #Have to transform coordinate system, x and y
+        #are different for this bounding box.
         left_top = (y, x)
         right_bottom = (y+h, x+w)
-        # left_top = (200, 400)
-        # right_bottom = (400, 600)
         return left_top, right_bottom
 
     def run(self):
@@ -95,7 +73,76 @@ class StreetSignRecognizer(object):
                 cv2.imshow('binary_window', self.binary_image)
                 cv2.waitKey(5)
             r.sleep()
+#End of StreetSignRecognizer Class
+
+# I created this class to break out the hue and saturation adjustments
+# They were unncecessary when running the main program, and made
+# It unnecessarily ugly and full of methods.
+class HSVSelector(object):
+    """ Designed to help find ideal HSV values
+        opens a binarized window with sliders to adjust the spectrum"""
+
+    def __init__(self):
+        self.cv_image = None                        # the latest image from the camera
+        self.bridge = CvBridge()                    # used to convert ROS messages to OpenCV
+        cv2.namedWindow('video_window')
+        rospy.Subscriber("/camera/image_raw", Image, self.process_image)
+        cv2.namedWindow('binary_window')
+
+        self.hue_lower_bound = 0
+        self.hue_upper_bound = 50
+        self.saturation_lower_bound = 175
+        self.saturation_upper_bound = 255
+        cv2.createTrackbar('hue lower bound', 'binary_window', 0, 255, self.set_hue_lower_bound)
+        cv2.createTrackbar('hue upper bound', 'binary_window', 0, 255, self.set_hue_upper_bound)
+        cv2.createTrackbar('saturation lower bound', 'binary_window', 0, 255, self.set_saturation_lower_bound)
+        cv2.createTrackbar('saturation upper bound', 'binary_window', 0, 255, self.set_saturation_upper_bound)
+
+    #Collection of callback functions for sliders
+    def set_hue_lower_bound(self, val):
+        """ A callback function to handle the OpenCV slider to select the hue lower bound """
+        self.hue_lower_bound = val
+
+    def set_hue_upper_bound(self, val):
+        """ A callback function to handle the OpenCV slider to select the hue upper bound """
+        self.hue_upper_bound = val
+
+    def set_saturation_lower_bound(self, val):
+        """ A callback function to handle the OpenCV slider to select the saturation lower bound """
+        self.saturation_lower_bound = val
+
+    def set_saturation_upper_bound(self, val):
+        """ A callback function to handle the OpenCV slider to select the saturation bound """
+        self.saturation_upper_bound = val
+
+    def process_image(self, msg):
+        """ Process image messages from ROS and stash them in an attribute
+            called cv_image for subsequent processing """
+
+        #Connecting to the image publisher, then converting to HSV format
+        #Then using HSV to identify the yellow (representing in binary_image)
+        self.cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
+        self.hsv_image = cv2.cvtColor(self.cv_image, cv2.COLOR_BGR2HSV)
+        self.binary_image = cv2.inRange(self.cv_image,
+                (self.hue_lower_bound,self.saturation_lower_bound,100),
+                (self.hue_upper_bound,self.saturation_upper_bound,255))
+
+    def run(self):
+        """ The main run loop"""
+        rospy.init_node('HSV selector')
+        r = rospy.Rate(10)
+        while not rospy.is_shutdown():
+            if not self.cv_image is None:
+                print "here"
+                # creates a window and displays the image for X milliseconds
+                cv2.imshow('video_window', self.cv_image)
+                cv2.imshow('hsv_window', self.hsv_image)
+                cv2.imshow('binary_window', self.binary_image)
+                cv2.waitKey(5)
+            r.sleep()
+#End of HSVSelector Class
 
 if __name__ == '__main__':
     node = StreetSignRecognizer()
+    # node = HSVSelector()
     node.run()
--- scripts/template_matcher.py
+++ scripts/template_matcher.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 """
 This code determines which of a set of template images matches
@@ -8,6 +9,17 @@ an input image the best using the SIFT algorithm
 class TemplateMatcher(object):
 
     def __init__ (self, images, min_match_count=10, good_thresh=0.7):
+        """ Store template images, create the sift and matching objects
+            which will be used for processing images
+
+            images: dictionary of "template name":"template filename" pairs,
+                which is used to find and store the actual images
+            min_match_count: Designed to be the minimum number of keypoint
+                matches for a template to be considered a possible
+                match
+            good_thresh: Used for homography transformation - determines whether
+                or not keypoints will be kept in transformation
+        """
         self.signs = {} #maps keys to the template images
         self.kps = {} #maps keys to the keypoints of the template images
         self.descs = {} #maps keys to the descriptors of the template images
@@ -19,35 +31,48 @@ class TemplateMatcher(object):
         # for potential tweaking
         self.min_match_count = min_match_count
         self.good_thresh = good_thresh #use for keypoint threshold
+        self.matcher = cv2.BFMatcher()
 
-        #TODO: precompute keypoints for template images
+        self.get_template_keypoints(images)
+
+    def get_template_keypoints(self, template_imgs):
+        """ Finds keypoints in the template images, which means that you
+            don't have to constantly recompute them.
+
+            template_imgs: dictionary of "template name":"template filename" pairs
+        """
+        for k, filename in template_imgs.iteritems():
+            # load template sign images as grayscale
+            self.signs[k] = cv2.imread(filename,0)
+
+            # precompute keypoints and descriptors for the template sign
+            self.kps[k], self.descs[k] = self.sift.detectAndCompute(self.signs[k],None)
+
 
 
     def predict(self, img):
-        """
-        Uses gather predictions to get visual diffs of the image to each template
-        returns a dictionary, keys being signs, values being confidences
+        """ Uses gather predictions to get visual diffs of the image to each template
+            returns a dictionary, keys being signs, values being confidences
+
+            img: opencv image, to be matched to the templates
         """
         visual_diff = {}
-
-        # TODO: get keypoints and descriptors from input image using SIFT
-        #       store keypoints in variable kp and descriptors in des
+        #Find the keypoints for the images
+        kp, des = self.sift.detectAndCompute(img,None)
 
         for k in self.signs.keys():
-            #cycle trough templage images (k) and get the image differences
+            #cycle through templage images (k) and get the image differences
             visual_diff[k] = self._compute_prediction(k, img, kp, des)
 
         if visual_diff:
-            pass
-            # TODO: convert difference between images (from visual_diff)
-            #       to confidence values (stored in template_confidence)
+            #confidence will now be based on ratio of visual differences
+            #and will sum to 1.
+            total = sum([visual_diff[key] for key in visual_diff.keys()])
+            template_confidence = {key: visual_diff[key]/total for key in self.signs.keys()}
 
         else: # if visual diff was not computed (bad crop, homography could not be computed)
             # set 0 confidence for all signs
             template_confidence = {k: 0 for k in self.signs.keys()}
-            
-        #TODO: delete line below once the if statement is written
-        template_confidence = {k: 0 for k in self.signs.keys()}
 
         return template_confidence
 
@@ -59,15 +84,58 @@ class TemplateMatcher(object):
         kp: keypoints from scene image,   des: descriptors from scene image
         """
 
-        # TODO: find corresponding points in the input image and the templae image
-        #       put keypoints from template image in template_pts
-        #       put corresponding keypoints from input image in img_pts
+        # Get list of matches between keypoints based on destriptors
+        matches = self.matcher.knnMatch(self.descs[k],des,k=2)
+        # Convert into point form
+        matches = [(m.queryIdx, m.trainIdx) for m, n in matches]
 
+        # Convert those points to points findHomography can handle
+        template_pts = np.zeros((len(matches),2))
+        img_pts = np.zeros((len(matches),2))
+        for idx in range(len(matches)):
+            match = matches[idx]
+            template_pts[idx,:] = self.kps[k][match[0]].pt
+            img_pts[idx,:] = kp[match[1]].pt
 
-        #TODO: change img to img_T once you do the homography transform
-        visual_diff = compare_images(img, self.signs[k])
+        # Transform input image so that it matches the template image as well as possible
+        M, mask = cv2.findHomography(img_pts, template_pts, cv2.RANSAC, self.good_thresh)
+        img_T = cv2.warpPerspective(img, M, self.signs[k].shape[::-1])
+
+        #Find total differences
+        visual_diff = compare_images(img_T, self.signs[k])
         return visual_diff
 # end of TemplateMatcher class
 
 def compare_images(img1, img2):
-    return 0
+    #normalize the images
+    norm1 = (img1 - np.mean(img1))/np.std(img1)
+    norm2 = (img2 - np.mean(img2))/np.std(img2)
+
+    #computing root mean squared error between the two arrays
+    #Average error between normalized pixels
+    thing = np.sqrt(((norm1 - norm2) ** 2).mean())
+
+    #A perfect match is zero - the worse that match, the larger
+    #the error. So, invert it.
+    return 1/thing
+
+if __name__ == '__main__':
+    images = {
+        "uturn": '../images/uturn_box_small.png',
+        "left": '../images/leftturn_box_small.png',
+        "right": '../images/rightturn_box_small.png'
+        }
+
+    tm = TemplateMatcher(images)
+    scenes = [
+        "../images/uturn_scene.jpg",
+        "../images/leftturn_scene.jpg",
+        "../images/rightturn_scene.jpg"
+    ]
+
+    for filename in scenes:
+        scene_img = cv2.imread(filename, 0)
+        pred = tm.predict(scene_img)
+        print filename.split('/')[-1]
+        print pred
+        print '\n'
